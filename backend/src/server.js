300520159876f9b32647c56253c7d7c1eb786d5c
Manojit Das <manojitdas180306@gmail.com>
2025-05-04 11:55:38 +0530

    adding the cors origin

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from './routes/auth.route.js'
 import userRoutes from './routes/user.route.js'
@@ -10,6 +11,10 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT
 
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());    
 
