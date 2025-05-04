23ea627e69885ddc24449807064b21a24bc24f89
Manojit Das <manojitdas180306@gmail.com>
2025-05-04 11:56:20 +0530

    Adding router and tanstack query setup with pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import OnboardingPage from "./pages/OnboardingPage";
+import CheckPage from "./pages/CheckPage";
+import NotificationPage from "./pages/NotificationPage";
+import CallPage from "./pages/CallPage";
+import ChatPage from "./pages/ChatPage";
+import PageNotFound from "./pages/PageNotFound";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Toaster } from "react-hot-toast";
+import { useEffect,useState } from "react";
+import {useQuery} from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios.js";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
 
-export default App
+  // tanstack query
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const response = await axiosInstance.get("/auth/check-auth");
+      return response.data;
+    },
+    retry: false,
+  })
+
+  console.log(data)
+
+  return ( 
+    <div data-theme="night" className="h-screen">
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/signup" element={<SignUpPage/>} />
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/onboarding" element={<OnboardingPage/>} />
+          <Route path="/check" element={<CheckPage/>} />
+          <Route path="/notification" element={<NotificationPage/>} />
+          <Route path="/call" element={<CallPage/>} />
+          <Route path="/chat" element={<ChatPage/>} />
+          <Route path="*" element={<PageNotFound/>} />
+      </Routes>
+
+      <Toaster/>
+
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'> 
+        <h2 className='text-2xl font-bold'>404 Page Not Found</h2>
+    </div>
+  )
+}
+
+export default PageNotFound
